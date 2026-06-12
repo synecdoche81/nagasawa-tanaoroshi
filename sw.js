5f3e8ea81dfd11f0ac1a95a987e78fceb06ff45e
synecdoche81 <nagajun81850928@gmail.com>
2026-06-12 14:59:37 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,40 @@
+var CACHE = 'tanaoroshi-v1';
+var ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon-180.png',
+  './icon-192.png',
+  './icon-512.png',
+  'https://cdn.jsdelivr.net/npm/@zxing/library@0.18.6/umd/index.min.js'
+];
+
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(function (c) { return c.addAll(ASSETS); })
+      .then(function () { return self.skipWaiting(); })
+  );
+});
+
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(keys.filter(function (k) { return k !== CACHE; })
+        .map(function (k) { return caches.delete(k); }));
+    }).then(function () { return self.clients.claim(); })
+  );
+});
+
+self.addEventListener('fetch', function (e) {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request).then(function (res) {
+      var clone = res.clone();
+      caches.open(CACHE).then(function (c) { c.put(e.request, clone); }).catch(function () {});
+      return res;
+    }).catch(function () {
+      return caches.match(e.request);
+    })
+  );
+});
